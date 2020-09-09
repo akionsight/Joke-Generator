190cb31d1b6e5da348c0ee03e85add3cc70031de
AkIonSight <akionsight@gmail.com>
2020-09-09 09:05:55 +0530

    minor changes

--- joke.py
+++ joke.py
@@ -1,7 +1,6 @@
 import requests
 import json
-joke_as_str = requests.get('https://official-joke-api.appspot.com/random_joke').text
-joke = json.loads(joke_as_str)
+joke = requests.get('https://official-joke-api.appspot.com/random_joke').json()
 setup = joke['setup']
 punchline = joke['punchline']
 print(setup)
